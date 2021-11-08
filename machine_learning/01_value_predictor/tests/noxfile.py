179d32045f2733724d3219aa9dc5d956224d794f
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-11-08 16:14:27 +0100

    add locust and pact

--- machine_learning/01_value_predictor/tests/noxfile.py
+++ machine_learning/01_value_predictor/tests/noxfile.py
@@ -15,5 +15,5 @@ def tests(session):
     # tests = ["test_locust.py", "test_unit_pytest.py", "test_flask_pytest.py"]
     # session.run("pytest", *tests)
     session.conda_install("--channel=conda-forge", "locust")
-    locust_params = "--headless -u 1 -r 1 --run-time 5s -f test_locust.py"
-    session.run("locust", "--headless -r 1 -u 1 -t 5s -f .\locustfile.py")
+    locust_args = r"--headless -r 1 -u 1 -t 5s -f .\locustfile.py"
+    session.run("locust", locust_args)
