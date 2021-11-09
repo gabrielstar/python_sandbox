8d208bca1c422f8ec9e4e0454ae9c0bcac50579b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-11-09 10:59:29 +0100

    added quick potato tests with nox

--- machine_learning/01_value_predictor/tests/test_quick_potato.py
+++ machine_learning/01_value_predictor/tests/test_quick_potato.py
@@ -1,13 +1,16 @@
 from QuickPotato import performance_test as pt
 import pytest
 
-
+"""
+    Performance Unit Tests
+    https://github.com/JoeyHendricks/QuickPotato
+"""
 sleepTimes = [0.2, 0.5, 0.7]
 
 
 @pytest.mark.parametrize("sleepTime", sleepTimes)
 def test_performance_of_function_predict(sleepTime, predict):
-    pt.test_case_name = f"Dummy-{sleepTime}"
+    pt.test_case_name = f"test_performance_of_function_predict-{sleepTime}"
     pt.max_and_min_boundary_for_average = {"max": float(0.8), "min": float(0.1)}
     pt.measure_method_performance(
         method=predict,  # <-- The Method which you want to test.
