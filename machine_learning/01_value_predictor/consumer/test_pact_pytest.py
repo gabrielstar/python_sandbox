4ab2f68ff644fe901b086b33debd21d69af385a7
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-11-10 15:31:46 +0100

    small pact changes

--- machine_learning/01_value_predictor/consumer/test_pact_pytest.py
+++ machine_learning/01_value_predictor/consumer/test_pact_pytest.py
@@ -1,14 +1,15 @@
 import atexit
 import requests
-from pact import Consumer, Provider, Like, Format
+from pact import Consumer, Provider, Like, Format, Verifier
+import subprocess
 
 pact = Consumer("Consumer").has_pact_with(Provider("Provider"))
 pact.start_service()
 atexit.register(pact.stop_service)
 
+
 """
     Consumer tests acdcording to the contract so it is sure that when it switches to real service all works
-
     1.Define the Consumer and Provider objects that describe API endpoint and expected payload
     2.Define the setup criteria for the Provider
     3.Define the Consumer request using Pact
@@ -17,8 +18,10 @@ atexit.register(pact.stop_service)
 """
 
 
-def test_get_simple_prediction(request_body):
-    # this is provider's contract - that thing will be returned, can be taekn form pact file
+def test_consumer_get_simple_prediction(request_body):
+    """
+        Generates the consumer-provider.json contract
+    """
     expected = {
         "errors": [],
         "id": Format().uuid,
@@ -35,10 +38,23 @@ def test_get_simple_prediction(request_body):
             headers={"Content-Type": "application/json"},
         )
         .will_respond_with(
-            status=200, headers={"Content-Type": "application/json"}, body=expected
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=expected,
         )
     )
 
     with pact:
-        result = requests.post(f"{pact.uri}/api", json=request_body)
+        requests.post(f"{pact.uri}/api", json=request_body)
         pact.verify()
+
+
+def test_provider_with_generated_contract(baseURL="http://localhost:5000"):
+    """
+        Verifies the consumer-provider.json contract against the  provider URL
+        Provider can be sent the contract and continuously check their side
+    """
+
+    verifier = Verifier(provider="PredictorApp", provider_base_url=baseURL)
+    success, logs = verifier.verify_pacts("consumer-provider.json")
+    assert success == 0
